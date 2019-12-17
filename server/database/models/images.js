7a649deafd661f5180f3f32e08b9939bfb97bd35
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-12-18 06:08:18 +0700

    return image as string ut8

--- server/database/models/images.js
+++ server/database/models/images.js
@@ -1,7 +1,12 @@
 
 module.exports = (sequelize, DataTypes) => {
   const images = sequelize.define('images', {
-    data: DataTypes.BLOB('tiny'),
+    data: {
+      type: DataTypes.BLOB('long'),
+      get() {
+        return this.getDataValue('data').toString('utf8'); // or whatever encoding is right
+      },
+    },
   }, {});
   images.associate = function(models) {
     // associations can be defined here
